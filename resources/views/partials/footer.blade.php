5b3648689897a7106fcc115118c9d57e52247d72
Jimmy Cook <jimmy.cook.1993@gmail.com>
2016-03-10 12:18:14 +0000

    Updated prototype, still unfinished

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,31 @@
+<div class="" style="height: 300px;">
+
+</div>
+<div class="Footer__container">
+	<div class="container">
+		<div class="col-md-3">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/about">About</a></li>
+				<li><a href="/contact">Contact</a></li>
+			</ul>
+		</div>
+		<div class="col-md-3">
+			<ul>
+				<li><a href="/past">Past</a></li>
+				<li><a href="/present">Present</a></li>
+				<li><a href="/future">Future</a></li>
+			</ul>
+		</div>
+		<div class="col-md-3">
+			<ul>
+				<li><a href="/">Twitter</a></li>
+				<li><a href="/">Facebook</a></li>
+				<li><a href="/">Petition</a></li>
+			</ul>
+		</div>
+		<div class="col-md-3 Footer__copyright">
+			<span>&copy; <?PHP echo date("Y"); ?> Royal Dunfermine Tombs</span>
+		</div>
+	</div>
+</div>
--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -5,10 +5,12 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf_token" content="{{ csrf_token() }}">
-	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
-	<title>@yield('title')</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400' rel='stylesheet' type='text/css'>
+
+	<title>@yield('title', 'Royal Dunfermline')</title>
+
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
@@ -20,7 +22,7 @@
 
 </head>
 <body id="app">
-					
+
 	@yield('content')
 
 </body>
